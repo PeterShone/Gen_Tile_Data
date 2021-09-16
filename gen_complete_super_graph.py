05a3e2c90ffff25ef4d5a87e7d3f2f45dd73b3f4
petershone <515663728@qq.com>
2021-09-16 10:55:43 +0800

    Update gen_complete_super_graph.py

--- gen_complete_super_graph.py
+++ gen_complete_super_graph.py
@@ -13,7 +13,6 @@ if __name__ == '__main__':
     debugger = MyDebugger(f"gen_tile_graph_{config.env_name}", fix_rand_seed=0, save_print_to_file=False)
     data_env = config.environment
     print('start!')
-    # logging.error('start')
 
     # the resulting tiles
     result_tiles = [data_env.proto_tiles[0]]
@@ -22,11 +21,9 @@ if __name__ == '__main__':
     
     for i in range(0, 21):
         print(f"computing ring_{i}")
-        # logging.error(f"computing ring_{i}")
         last_ring_num = len(last_ring)
         for last_ring_idx in range(last_ring_num):
             print(f"last ring_{last_ring_idx}")
-            # logging.error(f"last ring_{last_ring_idx}")
             last_layer_tile = last_ring.pop(0)
             for align_tile in data_env.proto_tiles:
                 neighbour_tiles, _ = get_all_tiles(last_layer_tile, align_tile, integer_align = True)
@@ -39,7 +36,6 @@ if __name__ == '__main__':
         if(i >= 9):
             tiles = result_tiles
             print(f"{len(tiles)}  tiles created!")
-            # logging.error(f"{len(tiles)}  tiles created!")
             file_name = "complete_graph_ring{}.pkl".format(i+1)
             graph = TileGraph(data_env.tile_count, tiles = tiles, one_hot = True, proto_tiles= data_env.proto_tiles)
 
@@ -55,16 +51,11 @@ if __name__ == '__main__':
 
             # logging...
             print(f"done_{i}")
-            # logging.error(f"done_{i}")
             num_of_nodes, num_of_adj_edges, num_of_collide_edges = graph._get_graph_statistics()
             print(f"num_of_nodes : {num_of_nodes}")
-            # logging.error(f"num_of_nodes : {num_of_nodes}")
             print(f"num_of_adj_edges : {num_of_adj_edges}")
-            # logging.error(f"num_of_adj_edges : {num_of_adj_edges}")
             print(f"num_of_collide_edges : {num_of_collide_edges}")
-            # logging.error(f"num_of_collide_edges : {num_of_collide_edges}")
             print()
-            # logging.error("------")
 
         
         
